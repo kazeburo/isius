deead88636914c73c7d43422bb9b272eeb707385
Masahiro Nagano <kazeburo@gmail.com>
2021-04-09 17:33:27 +0900

    http

--- go.mod
+++ go.mod
@@ -6,5 +6,10 @@ require (
 	github.com/digineo/go-ping v1.0.1
 	github.com/gorilla/mux v1.8.0
 	github.com/jessevdk/go-flags v1.5.0
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.4 // indirect
 	github.com/lestrrat/go-server-starter-listener v0.0.0-20150507032651-00dd68592c85
+	github.com/pkg/errors v0.9.1
+	go.uber.org/zap v1.16.0
 )
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"math"
 	"net"
@@ -20,6 +24,7 @@ import (
 	ping "github.com/digineo/go-ping"
 	"github.com/gorilla/mux"
 	"github.com/jessevdk/go-flags"
+	"github.com/kazeburo/isius/accesslog"
 	ss "github.com/lestrrat/go-server-starter-listener"
 )
 
@@ -40,12 +45,17 @@ type res struct {
 }
 
 type commandOpts struct {
-	Version         bool          `short:"v" long:"version" description:"Show version"`
-	Listen          string        `short:"l" long:"listen" default:"0.0.0.0" description:"address to bind"`
-	Port            string        `short:"p" long:"port" default:"3000" description:"Port number to bind"`
-	ReadTimeout     time.Duration `long:"read-timeout" default:"30s" description:"timeout of reading request"`
-	WriteTimeout    time.Duration `long:"write-timeout" default:"90s" description:"timeout of writing response"`
-	ShutdownTimeout time.Duration `long:"shutdown-timeout" default:"1h"  description:"timeout to wait for all connections to be closed."`
+	Version          bool          `short:"v" long:"version" description:"Show version"`
+	Listen           string        `short:"l" long:"listen" default:"0.0.0.0" description:"address to bind"`
+	Port             string        `short:"p" long:"port" default:"3000" description:"Port number to bind"`
+	LogDir           string        `long:"access-log-dir" default:"" description:"directory to store logfiles"`
+	LogRotate        int64         `long:"access-log-rotate" default:"30" description:"Number of rotation before remove logs"`
+	LogRotateTime    time.Duration `long:"access-log-rotate-time" default:"24h" description:"Interval time between file rotation"`
+	ReadTimeout      time.Duration `long:"read-timeout" default:"30s" description:"timeout of reading request"`
+	WriteTimeout     time.Duration `long:"write-timeout" default:"90s" description:"timeout of writing response"`
+	ShutdownTimeout  time.Duration `long:"shutdown-timeout" default:"1h"  description:"timeout to wait for all connections to be closed."`
+	MountAPIOn       string        `long:"mount-api-on" description:"url path to mount api on"`
+	DefaultUserAgent string        `long:"default-user-agent" default:"isisu-monitor-agent" description:"default user-agent string for http monitor"`
 }
 
 func round(f float64) int64 {
@@ -100,8 +110,6 @@ func userErrorJSON(w http.ResponseWriter, e error) {
 }
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	log.Printf("%v", vars)
 	w.Write([]byte("OK\n"))
 }
 
@@ -283,6 +291,160 @@ func handleCheckTCP(w http.ResponseWriter, r *http.Request) {
 	outJSON(w, OK, fmt.Sprintf("duration:%f", duration.Seconds()))
 }
 
+func makeTransport(ip *net.IPAddr, port int, vhost string, timeout time.Duration) http.RoundTripper {
+	baseDialFunc := (&net.Dialer{
+		Timeout: timeout,
+	}).DialContext
+	network := "tcp4"
+	if strings.Index(ip.String(), ":") != -1 {
+		network = "tcp6"
+	}
+	dialFunc := func(ctx context.Context, _, _ string) (net.Conn, error) {
+		address := net.JoinHostPort(ip.String(), fmt.Sprintf("%d", port))
+		return baseDialFunc(ctx, network, address)
+	}
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	if vhost != "" {
+		servername, _, err := net.SplitHostPort(vhost)
+		if err != nil {
+			servername = vhost
+		}
+		tlsConfig = &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         servername,
+		}
+	}
+
+	return &http.Transport{
+		// inherited http.DefaultTransport
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           dialFunc,
+		IdleConnTimeout:       30 * time.Second,
+		TLSHandshakeTimeout:   timeout,
+		ExpectContinueTimeout: 1 * time.Second,
+		// self-customized values
+		ResponseHeaderTimeout: timeout,
+		TLSClientConfig:       tlsConfig,
+		ForceAttemptHTTP2:     true,
+	}
+}
+
+func makeHTTPCheckHandler(defaultUA string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		mainTimeout := monTimeout
+		if r.Header.Get("X-Timeout") != "" {
+			i, err := strconv.ParseInt(r.Header.Get("X-Timeout"), 10, 64)
+			if err != nil {
+				userErrorJSON(w, fmt.Errorf("Could not parse X-Timeout: %v", err))
+				return
+			}
+			mainTimeout = time.Second * time.Duration(i)
+		}
+
+		if vars["ip"] == "" {
+			userErrorJSON(w, fmt.Errorf("No IP Address Specified"))
+			return
+		}
+		if vars["port"] == "" {
+			userErrorJSON(w, fmt.Errorf("No Port number Specified"))
+			return
+		}
+		port, err := strconv.Atoi(vars["port"])
+		if err != nil {
+			userErrorJSON(w, fmt.Errorf("Could not parse port number: %v", err))
+			return
+		}
+
+		ip, err := parseIP(vars["ip"])
+		if err != nil {
+			userErrorJSON(w, fmt.Errorf("Could not parse IP: %v", err))
+			return
+		}
+
+		if vars["status"] == "" {
+			userErrorJSON(w, fmt.Errorf("No Status code Specified"))
+			return
+		}
+		expectedStatus, err := strconv.Atoi(vars["status"])
+		if err != nil {
+			userErrorJSON(w, fmt.Errorf("Could not parse status number: %v", err))
+			return
+		}
+
+		if vars["method"] == "" {
+			userErrorJSON(w, fmt.Errorf("No HTTP method code Specified"))
+			return
+		}
+		method := strings.ToUpper(vars["method"])
+
+		vhost := vars["host"]
+		if vhost == "-" {
+			vhost = ip.String()
+		}
+		host := net.JoinHostPort(vhost, fmt.Sprintf("%d", port))
+		path := vars["path"]
+		path = "/" + path
+
+		schema := "http"
+		if vars["http_scheme"] == "check_https" {
+			schema = "https"
+		}
+		uri := fmt.Sprintf("%s://%s%s", schema, host, path)
+		if r.URL.RawQuery != "" {
+			uri += "?" + r.URL.RawQuery
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), mainTimeout)
+		defer cancel()
+
+		var b bytes.Buffer
+		req, err := http.NewRequestWithContext(
+			ctx,
+			method,
+			uri,
+			&b,
+		)
+		if err != nil {
+			userErrorJSON(w, fmt.Errorf("Failed create request: %v", err))
+			return
+		}
+		ua := r.UserAgent()
+		if ua == "" {
+			ua = defaultUA
+		}
+		req.Header.Set("User-Agent", ua)
+
+		transport := makeTransport(ip, port, vars["host"], mainTimeout)
+		client := &http.Client{
+			Transport: transport,
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+		}
+		start := time.Now()
+		res, err := client.Do(req)
+		duration := time.Since(start)
+
+		if err != nil {
+			outJSON(w, CRITICAL, fmt.Sprintf("duration:%f", duration.Seconds()), err)
+			return
+		}
+
+		defer res.Body.Close()
+		_, err = io.Copy(ioutil.Discard, res.Body)
+		if res.StatusCode != expectedStatus {
+			outJSON(w, CRITICAL, fmt.Sprintf("duration:%f", duration.Seconds()), fmt.Errorf("status code %d not match %d", res.StatusCode, expectedStatus))
+			return
+		}
+
+		outJSON(w, OK, fmt.Sprintf("duration:%f", duration.Seconds()))
+	}
+}
+
 func printVersion() {
 	fmt.Printf(`isius %s
 Compiler: %s %s
@@ -309,19 +471,33 @@ func _main() int {
 		return OK
 	}
 
+	mount := opts.MountAPIOn
+	if mount != "" {
+		mount = "/" + mount
+	}
+
+	handleHTTPCheck := makeHTTPCheckHandler(opts.DefaultUserAgent)
+
 	m := mux.NewRouter()
 	m.Handle("/live", http.HandlerFunc(handleHello))
-	m.Handle("/check_ping/{ip}", http.HandlerFunc(handleCheckPing))
-	m.Handle("/check_ping/{count:[0-9]+}/{interval:[0-9]+}/{timeout:[0-9]+}/{ip}", http.HandlerFunc(handleCheckPing))
+	m.Handle(mount+"/check_ping/{ip}", http.HandlerFunc(handleCheckPing))
+	m.Handle(mount+"/check_ping/{count:[0-9]+}/{interval:[0-9]+}/{timeout:[0-9]+}/{ip}", http.HandlerFunc(handleCheckPing))
 
-	m.Handle("/check_tcp/{ip}/{port:[0-9]+}", http.HandlerFunc(handleCheckTCP))
+	m.Handle(mount+"/check_tcp/{ip}/{port:[0-9]+}", http.HandlerFunc(handleCheckTCP))
 
-	m.Handle("/{http_scheme:check_https?}/{method:(?:GET|HEAD|get|head)}/{ip}/{port:[0-9]+}/{status:[0-9][0-9][0-9]}", http.HandlerFunc(handleHello))
-	m.Handle("/{http_scheme:check_https?}/{method:(?:GET|HEAD|get|head)}/{ip}/{port:[0-9]+}/{status:[0-9][0-9][0-9]}/{host}", http.HandlerFunc(handleHello))
-	m.Handle("/{http_scheme:check_https?}/{method:(?:GET|HEAD|get|head)}/{ip}/{port:[0-9]+}/{status:[0-9][0-9][0-9]}/{host}/{path:.*}", http.HandlerFunc(handleHello))
+	m.Handle(mount+"/{http_scheme:check_https?}/{method:(?:GET|HEAD|get|head)}/{ip}/{port:[0-9]+}/{status:[0-9][0-9][0-9]}", http.HandlerFunc(handleHTTPCheck))
+	m.Handle(mount+"/{http_scheme:check_https?}/{method:(?:GET|HEAD|get|head)}/{ip}/{port:[0-9]+}/{status:[0-9][0-9][0-9]}/{host}", http.HandlerFunc(handleHTTPCheck))
+	m.Handle(mount+"/{http_scheme:check_https?}/{method:(?:GET|HEAD|get|head)}/{ip}/{port:[0-9]+}/{status:[0-9][0-9][0-9]}/{host}/{path:.*}", http.HandlerFunc(handleHTTPCheck))
+
+	al, err := accesslog.New(opts.LogDir, opts.LogRotate, opts.LogRotateTime)
+	if err != nil {
+		log.Printf("Error in init logger: %v", err)
+		return CRITICAL
+	}
+	handler := al.WrapHandleFunc(m)
 
 	server := http.Server{
-		Handler:      m,
+		Handler:      handler,
 		ReadTimeout:  opts.ReadTimeout,
 		WriteTimeout: opts.WriteTimeout,
 	}
